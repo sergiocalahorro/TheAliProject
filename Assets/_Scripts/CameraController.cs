1649c9e537bea360d74280edb8405d8e73dde5f3
Sergio Calahorro Ureña <sergiocalahorrou27@gmail.com>
2020-12-15 18:51:40 +0100

    Initial commit

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 _offset = new Vector3(0f, 1f, -10f);
+    
+    public Transform player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 lookAt = player.position + _offset;
+        transform.position = lookAt;
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField, Range(1, 10)]
+    private float _speed = 5f;
+    [SerializeField, Range(1, 20)]
+    private float _jumpForce = 12f;
+    private float _groundCheckRadius = 0.2f;
+    private bool _facingRight = true;
+    private bool _grounded = true;
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private LayerMask _groundLayerMask;
+
+    public Transform groundCheckOrigin;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+
+        _groundLayerMask = LayerMask.GetMask("Ground");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        GroundCheck();
+    }
+
+    /// <summary>
+    /// Player's movement
+    /// </summary>
+    private void Move()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        float move = _speed * direction;
+        
+        // Flip character
+        if (move > 0f && !_facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0f && _facingRight)
+        {
+            Flip();
+        }
+        
+        transform.Translate(move * Time.deltaTime, 0f, 0f);
+        _animator.SetFloat(Constants.SPEED_F, Mathf.Abs(direction));
+    }
+
+    /// <summary>
+    /// Flip character according to the direction he's moving
+    /// </summary>
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        
+        Vector3 scale = transform.localScale;
+        scale.x *= -1f;
+        transform.localScale = scale;
+    }
+
+    /// <summary>
+    /// Player's jump
+    /// </summary>
+    private void Jump()
+    {
+        if (_grounded)
+        {
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    /// <summary>
+    /// Check if player is or not grounded
+    /// </summary>
+    private void GroundCheck()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll((Vector2) groundCheckOrigin.position,
+                                                            _groundCheckRadius, 
+                                                            _groundLayerMask);
+        
+        if (colliders.Length > 0)
+        {
+            _grounded = true;
+        }
+        else if (colliders.Length == 0)
+        {
+            _grounded = false;
+        }
+    }
+}
